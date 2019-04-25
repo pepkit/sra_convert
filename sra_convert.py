6f3c21fbcc71fe40bc2c48af7e740f9d6fd51923
Vince Reuter <vince.reuter@gmail.com>
2019-04-25 17:01:26 -0400

    add opt for removing original file

--- sra_convert.py
+++ sra_convert.py
@@ -5,6 +5,13 @@ import os
 import pypiper
 import sys
 
+
+CONVERT = "convert"
+DELETE_SRA = "delete_sra"
+DELETE_BAM = "delete_bam"
+REMOVAL_OPTNAME = "--remove"
+
+
 def _parse_cmdl(cmdl):
 	parser = ArgumentParser(description="Automatic GEO SRA data downloader")
 	
@@ -20,39 +27,41 @@ def _parse_cmdl(cmdl):
 			"[Default: $SRARAW:" + safe_echo("SRARAW") + "]")
 
 	parser.add_argument(
-			"-m", "--mode", default="convert",
-			help="Mode. Can be: 'convert', 'delete_sra', 'delete_bam'")
-	
+			"-m", "--mode", choices=[CONVERT, DELETE_SRA, DELETE_BAM], default=CONVERT,
+			help="Subcommand action to be performed")
+
+	parser.add_argument(REMOVAL_OPTNAME, action="store_true", help="Once converted, remove original file.")
+
 	# parser.add_argument(
 	# 		"--picard", dest="picard_path", default=safe_echo("PICARD"),
 	# 		help="Specify a path to the picard jar, if you want to convert "
 	# 		"fastq to bam [Default: $PICARD:" + safe_echo("PICARD") + "]")
 	
 	parser.add_argument(
-			"-r", "--srr", required=True, nargs="+",
-			help="SRR files")
+			"-r", "--srr", required=True, nargs="+", help="SRR files")
 
-	parser = pypiper.add_pypiper_args(parser, groups=["pypiper", "config"],
-		args=["output-parent"])
+	parser = pypiper.add_pypiper_args(
+		parser, groups=["pypiper", "config"], args=["output-parent"])
 	return parser.parse_args(cmdl)
+
 
 def safe_echo(var):
 	""" Returns an environment variable if it exists, or an empty string if not"""
 	return os.getenv(var, "")
 
 
-#def main(cmdl):
-
 if __name__ == "__main__":
-	#main(sys.argv[1:])
-	cmdl = sys.argv[1:]
-	args = _parse_cmdl(cmdl)
+	args = _parse_cmdl(sys.argv[1:])
 
-	key=args.srr[0]
+	def do_rm():
+		return getattr(args, REMOVAL_OPTNAME, None)
+
+	if do_rm() and args.mode != CONVERT:
+		print("Warning: {rm} applies only for mode {conv} (not {curr})".format(rm=REMOVAL_OPTNAME, conv=CONVERT, curr=args.mode))
+
+	key = args.srr[0]
 	outfolder = os.path.join(args.srafolder, "sra_convert_pipeline")
-	pm = pypiper.PipelineManager(	name="sra_convert",
-									outfolder=outfolder,
-									args=args)
+	pm = pypiper.PipelineManager(name="sra_convert", outfolder=outfolder, args=args)
 
 	nfiles = len(args.srr)
 	for i in range(nfiles):
@@ -60,26 +69,22 @@ if __name__ == "__main__":
 		infile = args.srr[i]
 		srr_acc = os.path.splitext(os.path.basename(args.srr[i]))[0]
 		outfile = os.path.join(args.bamfolder, srr_acc + ".bam")
-		if (not os.path.isfile(infile)):
+		if not os.path.isfile(infile):
 			infile = os.path.join(args.srafolder, args.srr[i] + ".sra")
 			outfile = os.path.join(args.bamfolder, args.srr[i] + ".bam")
-		if (not os.path.isfile(infile)):
-			next
 
-		if args.mode == "convert":
+		if args.mode == CONVERT:
 			cmd = "sam-dump -u {data_source} | samtools view -bS - > {outfile}".format(
 				data_source=infile, outfile=outfile)
 			target = outfile
-			pm.run(cmd, target=target)
-		elif args.mode == "delete_sra":
+			pm.run(cmd, target=target, follow=(lambda: os.unlink(infile)) if do_rm() else None)
+		elif args.mode == DELETE_SRA:
 			cmd = "rm {data_source}".format(data_source=infile)
-			target = "rm_"+infile
+			target = "rm_" + infile
 			pm.run(cmd, target=target, nofail=True)
-		elif args.mode == "delete_bam":
+		elif args.mode == DELETE_BAM:
 			cmd = "rm {data_source}".format(data_source=outfile)
-			target = "rm_"+outfile
+			target = "rm_" + outfile
 			pm.run(cmd, target=target, nofail=True)
-
-
 
 	pm.stop_pipeline()
